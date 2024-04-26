1fe18fdfab79739388e230de54bbacabf40ec50a
ahmed <a.elsayed.it@gmail.com>
2024-04-26 08:00:54 +0300

    Remove min bootstrap and use the normal version

--- includes/header.php
+++ includes/header.php
@@ -10,7 +10,7 @@ session_start();
 	<meta charset="UTF-8">
 	<meta name="viewport" content="width=device-width, initial-scale=1.0">
 	<!-- Bootstrap CSS -->
-	<link rel="stylesheet" href="assets/css/bootstrap.min.css">
+	<link rel="stylesheet" href="assets/css/bootstrap.css">
 	<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.5.0/font/bootstrap-icons.css">
 
 </head>
